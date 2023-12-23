ab19786692128317c3658d9222d83816d8f460f6
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2023-12-23 12:09:33 -0800

    gw@3z

--- sort.h
+++ sort.h
@@ -1,39 +1,32 @@
-fndef SORT_H
-#define SORT_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+nclude "sort.h"
 
 /**
- *  * struct listint_s - Doubly linked list node
- *   * @n: Integer stored in the node
- *    * @prev: Pointer to the previous element of the list
- *     * @next: Pointer to the next element of the list
- *      */
-typedef struct listint_s
+ *  * bubble_sort - Function that sorts an array of ints
+ *   * using bubble sort algorithm.
+ *    *
+ *     * @array: array of ints
+ *      * @size: size of array
+ *       */
+void bubble_sort(int *array, size_t size)
 {
-	    int n;
-	        struct listint_s *prev;
-		    struct listint_s *next;
-} listint_t;
+	    int temp;
+	        size_t i, k;
 
-/* print */
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+		    if (!array || size == 0)
+			            return;
 
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void merge_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-
-#endif
+		        for (i = 0; i < size - 1; i++)
+				    {
+					            for (k = 0; k < size - 1; k++)
+							            {
+									                if (array[k] > array[k + 1])
+												            {
+														                    temp = array[k];
+																                    array[k] = array[k + 1];
+																		                    array[k + 1] = temp;
+																				                    print_array(array, size);
+																						                }
+											        }
+						        }
+}
 
