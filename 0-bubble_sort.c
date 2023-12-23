68080e9202a87dbc8c18c982ed2a80b816f746de
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2023-12-23 07:44:03 -0800

    gw@3z

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,14 @@
-nclude "sort.h"
+uth: Brennan D Baraban
+ */
 
-/**
- *  * swap_ints - Swap two integers in an array.
- *   * @a: The first integer to swap.
- *    * @b: The second integer to swap.
- *     */
-void swap_ints(int *a, int *b)
+#include "sort.h"
+
+ /**
+  *  * swap_ints - Swap two integers in an array.
+  *   * @a: The first integer to swap.
+  *    * @b: The second integer to swap.
+  *     */
+ void swap_ints(int *a, int *b)
 {
 		int tmp;
 
